f406135a3dd4c3eb2f0fc8c33290f34c7b5b6044
Bintang Adiputra Pratama <bintangadiputra@proton.me>
2025-09-23 14:14:02 +0700

    touch up edit page

--- pages/post/edit.php
+++ pages/post/edit.php
@@ -28,35 +28,48 @@ $layout = new HTML('The Void: Edit Postingan');
 // TODO: Retain form values after refresh
 ?>
 
-<h2>Memposting sebagai <b><?= h($user['name']) ?></b> <?= h('@' . $user['username']) ?></h2>
-
-<hr />
-
-<form method="post" action="/post/edit">
-    <input type="hidden" name="id" value="<?= $postId ?>">
-    <input type="hidden" name="parent_id" value="<?= $post['parent_id'] ?? 0 ?>">
-
-    <?php if (!$post['parent_id']): ?>
-        <div class="flex">
-            <label for="private">Pribadi</label>
-            <input
-                type="checkbox"
-                id="private"
-                name="private"
-                <?php if ($post['private']): ?>
-                    checked
-                <?php endif; ?>
-            >
+<div class="flex-1">
+    <div class="border rounded-xs border-gray m-4 lg:m-8 px-1">
+        <div class="flex justify-between items-center h-10 px-1 py-1">
+            <div class=" flex items-center rounded-xs px-1 h-full">
+                <span class="font-bold"><?= h($user['name']) ?></span>
+                <span class="font-bold text-xl mx-1">·</span>
+                <?= h('@' . $user['username']) ?>
+            </div>
         </div>
-        <?php if ($errors['private'] ?? false): ?>
-            <div><?= h($errors['private']) ?></div>
-        <?php endif; ?>
-    <?php endif; ?>
 
-    <textarea class="block border" name="text"><?= h($post['text']) ?></textarea>
-    <?php if ($errors['text'] ?? false): ?>
-        <div><?= h($errors['text']) ?></div>
-    <?php endif; ?>
+        <hr class="text-gray" />
 
-    <button type="submit">EDIT</button>
-</form>
+        <form method="post" action="/post/edit">
+            <input type="hidden" name="id" value="<?= $postId ?>">
+            <input type="hidden" name="parent_id" value="<?= $post['parent_id'] ?? 0 ?>">
+
+            <div class="flex flex-col w-full px-4 py-4 justify-center items-center gap-4">
+                <?php if (!$post['parent_id']): ?>
+                    <div class="flex w-fit justify-start gap-2 items-center self-start">
+                        <input
+                            type="checkbox"
+                            id="private"
+                            name="private" 
+                            <?php if ($post['private']): ?>
+                                checked
+                            <?php endif; ?>
+                        />
+                        <label for="private" class="font-bold">Pribadi</label>
+                    </div>
+                    <?php if ($errors['private'] ?? false): ?>
+                        <div class="my-error"><?= h($errors['private']) ?></div>
+                    <?php endif; ?>
+                <?php endif; ?>
+
+                <textarea class="min-h-10 h-[50vh]" name="text" placeholder="Tulis postinganmu di sini..."
+                ><?= h($post['text']) ?></textarea>
+                <?php if ($errors['text'] ?? false): ?>
+                    <div class="my-error"><?= h($errors['text']) ?></div>
+                <?php endif; ?>
+
+                <button type="submit" class="my-button text-lg my-4">POST</button>
+            </div>
+        </form>
+    </div>
+</div>
